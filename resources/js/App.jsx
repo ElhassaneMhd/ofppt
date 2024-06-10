0092ee1bace2fa36180f0055d12a93d143084717
Zawalid <walid.zakan@gmail.com>
2024-06-10 14:42:45 +0100

    Fix login bug

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -15,9 +15,11 @@ createInertiaApp({
   resolve: (name) => {
     const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true });
     const page = pages[`./Pages/${name}.jsx`];
+
     page.default.layout =
       page.default.layout ||
       ((page) => <AppLayout>{['create', 'show', 'edit'].includes(page) ? page : page}</AppLayout>);
+
     return page;
   },
   setup({ el, App, props }) {
--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,13 +1,13 @@
 import { useForm } from '@/hooks/useForm';
-import { Button } from '../../components/ui';
+import { Button } from '@/components/ui';
 import { useTranslation } from 'react-i18next';
-import { useLogin, useSettings } from '@/hooks/useUser';
+import { useNavigate } from '@/hooks/useNavigate';
 
-export function Login() {
-    const { t } = useTranslation();
-    const { login, isLogging } = useLogin();
-    const { settings } = useSettings();
-    const {
+export default function Login() {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const {
     Form,
     options: { isValid, handleSubmit },
   } = useForm({
@@ -16,23 +16,30 @@ export function Login() {
       { name: 'email', type: 'email', label: t('form.email.label') },
       { name: 'password', type: 'password', label: t('form.password.label'), rules: { pattern: null } },
     ],
-    onSubmit: login,
+    onSubmit: () => {
+      navigate({
+        url: '/login',
+        method: 'POST',
+        data: { email: 'email', password: 'password' },
+      });
+    },
     submitOnEnter: true,
   });
 
   return (
-    <div className='relative flex h-full w-full flex-col justify-center gap-3 p-2 md:px-10 lg:px-20  '>
-      <h1 className='mb-8 text-2xl font-bold text-text-primary sm:text-3xl'>
-        {t('form.welcome')} {settings?.appName}
-      </h1>
+    <div className='relative flex h-full w-full flex-col justify-center gap-3 p-2 md:px-10 lg:px-20'>
+      <h1 className='mb-8 text-2xl font-bold text-text-primary sm:text-3xl'>Welcome</h1>
       {Form}
       <Button
         className={'my-4 w-full self-end'}
         disabled={!isValid}
-        onClick={() => !isLogging && handleSubmit()}
-        isLoading={isLogging}
+        onClick={() => handleSubmit()}
+        // isLoading={isLogging}
       >
-        {isLogging ? 'Logging In...' : t('form.login')}
+        {
+          // isLogging ? 'Logging In...' :
+          t('form.login')
+        }
       </Button>
 
       <p className='flex items-center justify-center gap-1 border-t border-border py-4 text-center text-text-primary'>
