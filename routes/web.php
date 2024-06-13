219b161a99df56d677ec5292b7264f961c13fa97
Winnery05 <elmedhassan2002@gmail.com>
2024-06-13 02:17:01 +0100

    fix routes bugs

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ArticlesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
@@ -9,23 +8,19 @@ use App\Http\Controllers\FilieresController;
 
 
 
-// Redirect to dashboard
 
 Route::get('/', function () {
     return redirect('/home');
 });
 Route::get('/home', [HomeController::class, 'index'])->name('home.home');
-Route::get('/{data}', [HomeController::class, 'dataPage'])->name('data.page');
-Route::get('/{data}/{id}', [HomeController::class, 'dataDetails'])->name('data.details');
+Route::get('/filieres', [FilieresController::class, 'userIndex'])->name('home.filieres');
+Route::get('/blog', [ArticlesController::class, 'userIndex'])->name('home.blog');
+Route::get('/events', [EventsController::class, 'userIndex'])->name('home.events');
 Route::inertia('/contact', 'Contact')->name('home.contact');
-
-Route::get('/admin', function () {
-    return redirect('/admin/dashboard');
-});
 
 Route::fallback(function () {
     return inertia('NotFound');
 });
 
 //require __DIR__.'/spatie.php';
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
